2ec0543c94adaee7ada25eab3242c75edbc9ec25
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-04-29 19:48:32 +0900

    unityでコントローラーのinputを受け取れるように処理を書いた

---
+++ unity/junction2017/Assets/Plugins/Framework/Utility/Util.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+
+public class Util{
+	public static void Normalize(Transform t)
+	{
+		t.localPosition = Vector3.zero;
+		t.localEulerAngles = Vector3.zero;
+		t.localScale = Vector3.one;
+	}
+
+	public static GameObject InstantiateTo(GameObject parent, GameObject go)
+	{
+		GameObject ins = (GameObject)GameObject.Instantiate(
+			go, 
+			Vector3.zero,
+			Quaternion.identity
+		);
+
+		ins.transform.SetParent (parent.transform, false);
+
+		//Normalize(ins.transform);
+		return ins;
+	}
+
+	public static GameObject InstantiateTo(GameObject parent, Prefab prefab)
+	{
+		return InstantiateTo(parent, prefab.gameObject);
+	}
+
+	public static T InstantiateTo<T>(GameObject parent, GameObject go)
+		where T : Component
+	{
+		return InstantiateTo(parent, go).GetComponent<T>();
+	}
+
+	public static T InstantiateTo<T>(GameObject parent, Prefab prefab)
+		where T : Component
+	{
+		return InstantiateTo(parent, prefab.gameObject).GetComponent<T>();
+	}
+
+	public static void DeleteAllChildren(Transform parent)
+	{
+		List<Transform> transformList = new List<Transform>();
+		foreach (Transform child in parent) transformList.Add(child);
+		parent.DetachChildren();
+		foreach (Transform child in transformList) GameObject.Destroy(child.gameObject);
+	}
+
+	public static AnimationClip LoadAnimationClip(string path){
+		AnimationClip clip = Resources.Load<AnimationClip>(path);
+		if (clip != null) {
+			return AnimationClip.Instantiate<AnimationClip>(clip);
+		}
+		return null;
+	}
+
+	public static float LoadAnimationSecond(Animator animator, string name){
+		RuntimeAnimatorController runtimeAnimatorController = animator.runtimeAnimatorController;
+		AnimationClip[] clips = runtimeAnimatorController.animationClips;
+		// 全てのレイヤーを取り出す
+		for(int i = 0; i < clips.Length; ++i) {
+			if (clips[i].name == name) {
+				return clips [i].length;
+			}
+		}
+		return 0f;
+	}
+
+	public static IEnumerator DelayedAction(float delaySecond, Action action){
+		yield return new WaitForSeconds (delaySecond);
+		action();
+	}
+}
---
+++ unity/junction2017/Assets/Scripts/InputManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct GamePadButton{
+	public float Horizontal;
+	public float Vertical;
+	public bool Y;
+	public bool X;
+	public bool A;
+	public bool B;
+}
+
+public class InputManager : SingletonBehaviour<InputManager> {
+	public Action<GamePadButton> OnPressButton;
+
+	void Update(){
+		if (OnPressButton != null) {
+			GamePadButton Gbutton = new GamePadButton ();
+			Gbutton.A = Input.GetButton ("Fire2");
+			Gbutton.X = Input.GetButton ("Fire1");
+			Gbutton.B = Input.GetButton ("Fire3");
+			Gbutton.Y = Input.GetButton ("Jump");
+			Gbutton.Horizontal = Input.GetAxis ("Horizontal");
+			Gbutton.Vertical = Input.GetAxis ("Vertical");
+			OnPressButton (Gbutton);
+		}
+	}
+}
